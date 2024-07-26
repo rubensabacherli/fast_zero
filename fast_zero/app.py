f18838ef48ec3781ca28659cd9f830f78583ee84
Rubens Abacherli Neto <rubensabacherli@gmail.com>
2024-07-25 21:13:17 -0300

    Início da terceira aula, processo de algun schemas sendo criados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,20 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    
+    return user
