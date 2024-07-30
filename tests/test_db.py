f0d74933f17ab129299171c5317c92a693079ab6
Rubens Abacherli Neto <rubensabacherli@gmail.com>
2024-07-30 19:42:37 -0300

    Adicionada a primeira migração com Alembic. Criando a tabela de usuários

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,18 @@
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user():
+def test_create_user(session):
     user = User(
         username='Rubens', email='rubens@rubens.com', password='123123'
     )
 
-    assert user.username == 'Rubens'
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(
+        select(User).where(User.email == 'rubens@rubens.com')
+    )
+
+    assert result.username == 'Rubens'
